1490b730c4d8e74fa25201a183b1e0ca011d6c88
刘东 <liud101@onewo.com>
2026-03-17 18:01:13 +0800

    初始提交后端项目

---
+++ src/main/java/com/liud/ainocodegenerator/AiNocodeGeneratorApplication.java
@@ -0,0 +1,14 @@
+package com.liud.ainocodegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiNocodeGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiNocodeGeneratorApplication.class, args);
+        System.out.println("server finish start");
+    }
+
+}
