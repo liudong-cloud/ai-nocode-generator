19b4b8167d2f7354e372fd17268bc35e22ea7a1f
刘东 <liud101@onewo.com>
2026-03-26 09:28:43 +0800

    前端项目修改，集成openapi2ts等工具

---
+++ ai-nocode-generator-front/src/components/GlobalFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="global-footer">
+    <div>
+      <a href="https://www.codefather.cn" target="_blank">编程导航原创项目 by 程序员Big东</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.global-footer {
+  text-align: center;
+  padding: 16px;
+  background: #efefef;
+}
+</style>
