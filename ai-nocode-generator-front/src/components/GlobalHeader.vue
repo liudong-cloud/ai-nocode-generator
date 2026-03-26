c33c7396fa714dfcd582dc653f6caebcb5f97312
刘东 <liud101@onewo.com>
2026-03-26 09:28:43 +0800

    前端项目修改，集成openapi2ts等工具

---
+++ ai-nocode-generator-front/src/components/GlobalHeader.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="globalHeader">
+    <a-row :wrap="false">
+      <a-col flex="200px">
+        <div class="title-bar">
+          <img class="logo" src="../assets/logo.png" alt="logo" />
+          <div class="title">AI 答题应用平台</div>
+        </div>
+      </a-col>
+      <a-col flex="auto">
+        <a-menu
+          v-model:selectedKeys="current"
+          mode="horizontal"
+          :items="items"
+          @click="doMenuClick"
+        />
+      </a-col>
+      <a-col flex="120px">
+        <div class="user-login-status">
+          <a-button type="primary" href="/user/login">登录</a-button>
+        </div>
+      </a-col>
+    </a-row>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { MenuProps } from 'ant-design-vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const items = ref<MenuProps['items']>([
+  {
+    key: '/',
+    label: '主页',
+    title: '主页',
+  },
+  {
+    key: '/about',
+    label: '关于我们',
+    title: '关于我们',
+  },
+])
+
+const current = ref<string[]>(['/'])
+
+// 路由跳转
+const doMenuClick = ({ key }: { key: string }) => {
+  router.push({
+    path: key,
+  })
+}
+</script>
+
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: black;
+  font-size: 18px;
+  margin-left: 16px;
+}
+
+.logo {
+  height: 48px;
+}
+</style>
---
+++ ai-nocode-generator-front/src/layouts/BasicLayout.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <GlobalFooter />
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GlobalHeader from '@/components/GlobalHeader.vue'
+import GlobalFooter from '@/components/GlobalFooter.vue'
+</script>
+
+<style scoped>
+#basicLayout .header {
+  padding-inline: 20px;
+  color: unset;
+  background: white;
+}
+
+#basicLayout .content {
+  background: linear-gradient(to right, #fefefe, #fff);
+  padding: 20px;
+}
+
+#basicLayout .footer {
+  padding: 16px;
+  position: sticky;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
